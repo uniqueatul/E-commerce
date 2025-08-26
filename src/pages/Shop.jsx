13573cf8339b13beebd945d67bee38e27d65367c
Atul Kumar Singh <136229949+uniqueatul@users.noreply.github.com>
2025-08-27 02:18:18 +0530

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -7,7 +7,7 @@ import NewCollections from "../components/NewCollections/NewCollections"
 
 
 import Footer from '../components/Footer/Footer'
-import Newsletter from '../components/NewsLetter/NewsLetter'
+import Newsletter from '../components/Newsletter/Newsletter'
 
 const Shop = () => {
   return (
@@ -17,7 +17,7 @@ const Shop = () => {
       <Offers />
       <NewCollections />
       <Newsletter/>
-      <Footer />
+      
     </div>
   )
 }
